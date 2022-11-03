3517609c4042cd3dddb2efc4f20278570ac86489
He110te4m <me@he110.site>
2022-11-03 17:31:06 +0800

    feat(AppPlugin): add plugins of APP

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import Footer from '~/components/Footer.vue'
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <RouterView />
---
+++ src/components/menu/src/NavMenu.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <Menu as="nav">
+    <MenuButton>
+      MenuTitle
+    </MenuButton>
+    <MenuItems as="dl">
+      <MenuItem as="dd">
+        <a>menu item</a>
+      </MenuItem>
+    </MenuItems>
+  </Menu>
+</template>
