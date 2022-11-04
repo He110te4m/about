2ccd3027a775d2fe568b5c2a9358e462fec0b948
He110te4m <me@he110.site>
2022-11-04 17:00:26 +0800

    feat(background): add App background

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
-import Footer from '~/components/Footer.vue'
+import AppFooter from '~/components/AppFooter.vue'
+import AppBackground from '~/components/AppBackground.vue'
 </script>
 
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <RouterView />
-    <Footer />
+    <AppFooter />
+    <AppBackground />
   </main>
 </template>
