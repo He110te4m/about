2337457cff438590201a491349d41c2373ba2a8f
He110te4m <i@he110.info>
2022-11-05 10:59:22 +0800

    refactor(directory): change layout directory

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script lang="ts" setup>
-import AppFooter from '~/components/AppFooter.vue'
-import AppBackground from '~/components/AppBackground.vue'
+import DefaultLayout from '~/layouts/default/layout.vue'
 </script>
 
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
-    <AppFooter />
-    <AppBackground />
-  </main>
+  <DefaultLayout />
 </template>
