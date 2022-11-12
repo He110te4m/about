705df8101e5ee404a022192992c9ba5296d71e46
He110te4m <i@he110.info>
2022-11-12 21:08:46 +0800

    ci: upadte github workflow

---
+++ src/layouts/default/Layout.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import AppFooter from './AppFooter.vue'
+import AppBackground from './AppBackground.vue'
+</script>
+
+<template>
+  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <RouterView />
+    <AppFooter />
+    <AppBackground />
+  </main>
+</template>
