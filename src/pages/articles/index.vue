e4558ba8d39ccc225e64cc5a2ba5cc82269c89a9
He110te4m <me@he110.site>
2022-11-28 15:41:02 +0800

    refactor(ArticleList): Reduce code complexity

--- src/pages/articles/index.vue
+++ src/pages/articles/index.vue
@@ -1,28 +1,8 @@
 <script lang="ts" setup>
-import { useFilterOptions } from './composables/useFilterOptions'
-import type { Article } from '~/components/article'
-import type { PagerItem } from '~/components/pager'
+import { useArticleList } from './composables/useArticleList'
 
 // Calculate the list to display
-const list = ref<Article[]>([])
-const { filterOptions } = useFilterOptions()
-const { list: filteredList } = useObjectListFilter(list, filterOptions)
-const limit = ref(5)
-const { list: articleList, page, maxPage, jump } = usePage(filteredList, limit)
-function onJump(item: PagerItem) {
-  if (item.type === 'page') {
-    jump(item.value)
-  }
-}
-
-// Handle data loading
-onMounted(() => {
-  const { data } = getArticleList()
-  watch(data, (articles = []) => {
-    articles.reverse()
-    list.value = articles
-  })
-})
+const { list, page, maxPage, onJump } = useArticleList()
 
 const router = useRouter()
 const route = useRoute()
@@ -37,6 +17,6 @@ function onChangeCategory(category: string) {
 </script>
 
 <template>
-  <ArticleList :list="articleList" @click-category="onChangeCategory" />
+  <ArticleList :list="list" @click-category="onChangeCategory" />
   <Pager :page="page" :max-page="maxPage" @jump="onJump" />
 </template>
