ed532acfb974e8f207e6ed15f7ce152d93d4f277
He110te4m <i@he110.info>
2022-12-01 20:51:52 +0800

    feat(ArticleDetail): create article detail page

--- src/pages/articles/index.vue
+++ src/pages/articles/index.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
 import { useArticleList } from './composables/useArticleList'
+import type { Article } from '~/apis/article'
 
 // Calculate the list to display
 const { list, page, maxPage, onJump } = useArticleList()
 
 const router = useRouter()
 const route = useRoute()
+function goArticleDetail(id: Article['id']) {
+  router.push({
+    path: `${route.path}/${id}`,
+  })
+}
 function updateRouteQuery(query: Record<string, string>) {
   router.push({
     path: route.path,
@@ -13,15 +19,20 @@ function updateRouteQuery(query: Record<string, string>) {
   })
 }
 
-function onClickCategory(category: string) {
+function filterArticleByCategory(category: string) {
   updateRouteQuery({ category })
 }
-function onClickTag(tag: string) {
+function filterArticleByTag(tag: string) {
   updateRouteQuery({ tag })
 }
 </script>
 
 <template>
-  <ArticleList :list="list" @click-category="onClickCategory" @click-tag="onClickTag" />
+  <ArticleList
+    :list="list"
+    @click-title="goArticleDetail"
+    @click-category="filterArticleByCategory"
+    @click-tag="filterArticleByTag"
+  />
   <Pager :page="page" :max-page="maxPage" @jump="onJump" />
 </template>
