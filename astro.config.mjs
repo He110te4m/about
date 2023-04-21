284376e889607e95811f351a78308403e3ff423b
He110te4m <me@he110.site>
2023-04-21 14:24:46 +0800

    feat: add auto depoly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
+// @ts-check
+
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import { netlifyFunctions as deployNetlify } from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://me.he110.site',
-	integrations: [mdx(), sitemap()],
+	site: 'https://astro.he110.site',
+  integrations: [
+    mdx(),
+    sitemap(),
+  ],
+  output: 'server',
+  adapter: deployNetlify(),
 });
