3c7aec4fa5ff9cf6e31645877b2364d70e5afc70
He110te4m <i@he110.info>
2023-07-11 20:51:53 +0800

    cleanup project

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'He110 \'s Blog'
-export const SITE_DESCRIPTION = '长松卧壑困风霜，时来屹立扶明堂。'
--- src/content/posts/Vite micro-service.md
+++
@@ -1,7 +0,0 @@
----
-title: "Vite 微前端实践"
-description: ""
-pubDate: 1502348230000
-category: "Front End"
-tags: ["micro-service", "vite"]
----
