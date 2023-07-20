8ea225724ea16ebeb87b1fc5def50965b2eaafc1
He110te4m <i@he110.info>
2023-07-20 22:07:26 +0800

    refactor(style): update article card style

--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -23,20 +23,15 @@ const list = useFilter(
 
 <template>
   <dl flex="~ col" items="center" gap="4">
-    <dd v-for="article in list" :key="article.url" max-w="720px" w="80%" b="1 color-gray-300" shadow="md color-gray-500">
+    <dd v-for="article in list" :key="article.url" max-w="720px" w="80%" flex items="center">
       <RouterLink v-slot="{ navigate }" :to="article.url" custom>
-        <div role="link" cursor="pointer" px="4" py="2" b-be="1 color-gray-200" @click="navigate">
+        <div role="link" cursor="pointer" text="lg" @click="navigate">
           {{ article.title }}
         </div>
-        <div flex justify="between" px="4" py="2">
-          <div>
-            {{ article.category }}
-          </div>
-          <RouterLink :to="article.url">
-            阅读全文
-          </RouterLink>
-        </div>
       </RouterLink>
+      <div scale="50" b="1 color-gray-500" px="4" b-rd="4">
+        {{ article.category }}
+      </div>
     </dd>
   </dl>
 </template>
