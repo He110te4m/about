4bf570c8355379991219174364b1b1c52fa6d238
He110te4m <i@he110.info>
2023-07-20 13:55:19 +0800

    fix: missing lookup import

--- src/utils/fp/record.ts
+++ src/utils/fp/record.ts
@@ -1,5 +1,6 @@
 import { getOrElse } from 'fp-ts/Option'
 import { constant, flow } from 'fp-ts/function'
+import { lookup } from 'fp-ts/Record'
 
 export function prop(propName: string, defaultValue = '') {
   return flow(
