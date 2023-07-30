24618105baa502882189fafd6566513d9423ca32
He110te4m <i@he110.info>
2023-07-30 20:14:31 +0800

    fix: wap style

--- src/components/layout/LayoutGroup.vue
+++ src/components/layout/LayoutGroup.vue
@@ -3,7 +3,7 @@
     <LayoutHeader h="40px" content-bg />
     <main py="8" overflow="y-auto" flex="1">
       <slot name="wrapper">
-        <div max-w="720px" w="80%" mx="auto" h="full" card>
+        <div max-w="720px" w="80%" mx="auto" min-h="full" card>
           <slot />
         </div>
       </slot>
