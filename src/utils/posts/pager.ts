ecf6c28d5da7fd81f6d871fe69b33d0874f69e31
He110te4m <i@he110.info>
2023-11-26 14:22:05 +0800

    feat(posts): support posts pager

--- src/utils/posts/pager.ts
+++ src/utils/posts/pager.ts
@@ -1,6 +1,9 @@
 import type { PostPagerOption } from './types'
 import type { PostInfo } from '~posts'
 
-export function makePostsPager(_options: PostPagerOption = {}) {
-  return (posts: PostInfo[]) => posts
+export function makePostsPager({ page = 1, size = 10 }: PostPagerOption = {}) {
+  const startIndex = (page - 1) * size
+  const endIndex = startIndex + size
+
+  return (posts: PostInfo[]) => posts.slice(startIndex, endIndex)
 }
