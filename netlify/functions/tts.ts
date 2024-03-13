1f4e250f4ff2889dbc92875d8a9a92896a861ce1
He110te4m <me@he110.site>
2024-03-13 17:05:31 +0800

    feat: test env var

--- netlify/functions/tts.ts
+++ netlify/functions/tts.ts
@@ -5,7 +5,7 @@ const api = express()
 
 const router = Router()
 router.get('/tts', async (req, res) => {
-  res.end('Hello tts')
+  res.end(`Hello tts ${process.env.HUGGINGFACE_API_KEY}`)
 })
 
 api.use('/api/', router)
